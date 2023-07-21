eca7cad74c43e0cea3dec20b3ada37767bdcdc85
Abhishek Choudhury <abhishek.choudhury2697@gmail.com>
2023-07-21 23:24:20 +0530

    add pizza logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from "react";
-import Alert, { AlertType } from "./components/Alert";
-import Button, { ButtonType } from "./components/Button";
 
 function App() {
-  const [showAlert, setShowAlert] = useState(false);
-  const type: ButtonType | AlertType = "primary";
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
+  });
+
+  const handleClick = () => {
+    // pizza.toppings.push("Cheese");
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+  };
+
   return (
     <>
-      {showAlert && (
-        <Alert
-          alert_type={type}
-          onClose={() => {
-            setShowAlert(false);
-            console.log(`${type} alert closed!`);
-          }}
-        >
-          <strong>Holy guacamole!</strong> You should check in on some of those
-          fields below.
-        </Alert>
-      )}
-      <Button
-        button_type={type}
-        onClick={() => {
-          console.log(`${type} button clicked!`);
-          setShowAlert(true);
-        }}
-      >
-        {type}
-      </Button>
+      <div>{JSON.stringify(pizza)}</div>
+      <button onClick={handleClick}></button>
     </>
   );
 }
